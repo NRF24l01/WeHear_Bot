709ebecaad277b1db72f710c567885a544665bed
$sudo heck beluga <s.nevrovsky@ya.ru>
2023-01-24 07:40:50 +0300

    Upload

--- bot_main.py
+++ bot_main.py
@@ -1,8 +1,18 @@
-
+#Import telegramApi
 import telebot
+from telebot import types
 
+#Import random
 from random import choice
 
+#Import token (if you have error on this line, create token.py and create label token like str)
 from token import token
 
+#Import sqllite for database
 import sqlite3
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message()
