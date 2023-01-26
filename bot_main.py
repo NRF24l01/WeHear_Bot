3a9a66f46dc442cf15a4a1793e4c7fa32fd0144b
$sudo heck beluga <96012285+NRF24l01@users.noreply.github.com>
2023-01-26 06:00:56 -1100

    Add files via upload

--- bot_main.py
+++ bot_main.py
@@ -1,13 +1,20 @@
 #Import telegramApi
-import telebot
-from telebot import types
+try:
+    from telebot import types
+    import telebot
+except:
+    import pip
+    pip.main(['install', 'PyTelegramBotAPI'])
 
 #Import random
 import random
 from random import choice
 
 #Import token (if you have error on this line, create teletoken.py and create label token like str)
-from teletoken import token as tele_token
+try:
+    from teletoken import token as tele_token
+except:
+    tele_token = input("Input token: ")
 
 #Import config
 import config
@@ -21,9 +28,25 @@ from datetime import datetime
 bot = telebot.TeleBot(tele_token)
 
 ks = telebot.types.ReplyKeyboardMarkup()
+ks.row("💸Купить💸")
 ks.row(" 🔎Информация 🔎", "☎️Звонок☎️")
 ks.row("👽Профиль👽","👣Регистрация👣")
-ks.row("💸Купить💸")
+ks.row("☢Разработчкики☢️")
+
+def data_upd(table, label, where, what_put, data_base = config.data_base):
+    pass
+
+def get_data(arg):
+    '''This function catch data by command.
+    In arg you put text with command(/mnasd 20)
+    This function return what user input(20)'''
+    return arg.split()[1:]
+
+@bot.message_handler(commands=["mname"])
+def mname(message):
+    name = get_data(message.text, reply_markup=ks)
+
+
 
 @bot.message_handler(commands=["start"])
 def start(message):
@@ -38,8 +61,15 @@ def text_f_u(message):
     res = current_datetime + " " + user_id + "  '" + message.text + "'"
     print(res)
     if message.text == "💸Купить💸":
-        bot.send_photo(message.from_user.id, photo=open('money.jpg','rb'), caption=config.def_buy)
+        try:
+            bot.send_photo(message.from_user.id, photo=open('money.jpg','rb'), caption=config.def_buy, reply_markup=ks)
+        except:
+            print("No such file: money.jpg")
     elif message.text == "👣Регистрация👣":
-        bot.send_message(message.from_user.id, config.def_reg)
+        bot.send_message(message.from_user.id, config.def_reg, reply_markup=ks)
+    elif message.text == "☢Разработчкики☢️":
+        bot.send_message(message.from_user.id, config.def_cr)
+    elif message.text == "🔎Информация 🔎":
+        bot.send_message(message.from_user.id, "Я жду текст сюда", reply_markup=ks)
 
 bot.polling(none_stop=True,non_stop=True)
